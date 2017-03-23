fc42fad7d704911a2d54f533bb33754aa4439514
Corey Wofford <msucorey@gmail.com>
2017-03-22 20:23:49 -0700

    make Heroku environment adjustments

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,33 +1,30 @@
-const path = require('path');
+var path = require('path');
 var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/rws.jsx',
+  entry: "./frontend/rws.jsx",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
   plugins:[
-  new webpack.DefinePlugin({
-    'process.env':{
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress:{
-      warnings: true
-    }
-  })
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
   ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
@@ -35,5 +32,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
 };
