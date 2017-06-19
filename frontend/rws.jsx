ab87dc1f139ec2115a54bdd468cdd7df7aa1eb0d
Corey Wofford <msucorey@gmail.com>
2017-06-19 10:28:47 -0400

    add comments for training demo

--- frontend/rws.jsx
+++ frontend/rws.jsx
@@ -4,6 +4,15 @@ import configureStore from './store/store';
 import Root from './components/root';
 import { fetchEvents } from './actions/event_actions';
 
+/*
+This is where the magic begins.
+Webpack(production) has already put this file into bundle.js inside the
+javascripts subfolder of app/assets.  From there, it gets loaded by Rails when
+the application.html.erb file is loaded.  Now, it's a simple matter of
+listening on the document for DECOMContentLoaded and then rendering the Root
+component to the 'root' DOM element.
+*/
+
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
